9b4e40431f944245f4e48ed7ac4d12908f505f0e
David <david.lima@bttis.com>
2025-05-19 13:48:13 -0300

    changes

--- src/infra/http/controllers/playlist/fetch-playlists.controller.ts
+++ src/infra/http/controllers/playlist/fetch-playlists.controller.ts
@@ -1,13 +1,24 @@
 import { Controller, Get, NotFoundException, Query } from '@nestjs/common'
 import { FetchPlaylists } from 'src/domain/usecases/playlist/fetch-playlists'
 import { PlaylistPresenter } from '../../presenters/playlist.presenter'
+import { z } from 'zod';
+import { ZodValidationPipe } from '../../pipes/zod-validation.pipe';
+
+const requestSchema =  z.object({
+  page: z.coerce.number().optional(),
+  limit: z.coerce.number().optional(),
+});
+  
+type RequestSchema = z.infer<typeof requestSchema>;
+
+const validationPipe = new ZodValidationPipe(requestSchema);
 
 @Controller('/playlist')
 export class FetchPlaylistsController {
   constructor(private readonly fetchPlaylists: FetchPlaylists) {}
 
   @Get()
-  async handle(@Query() query: { page?: number; limit?: number }) {
+  async handle(@Query(validationPipe) query: RequestSchema) {
     const { page, limit } = query
 
     const data = await this.fetchPlaylists.execute({
--- src/infra/http/controllers/songs/get-songs.controller.ts
+++ src/infra/http/controllers/songs/get-songs.controller.ts
@@ -3,13 +3,25 @@ import { Song } from 'src/domain/entities/songs'
 
 import { GetSongsUseCase } from 'src/domain/usecases/songs/get-songs'
 import { SongPresenter } from '../../presenters/song.presenter'
+import { z } from 'zod';
+import { ZodValidationPipe } from '../../pipes/zod-validation.pipe';
+
+const requestSchema =  z.object({
+  page: z.coerce.number().optional(),
+  limit: z.coerce.number().optional(),
+});
+  
+type RequestSchema = z.infer<typeof requestSchema>;
+
+const validationPipe = new ZodValidationPipe(requestSchema);
+
 
 @Controller('/song')
 export class GetSongsController {
   constructor(private readonly getSongUseCase: GetSongsUseCase) {}
 
   @Get()
-  async execute(@Query() query: { page?: number; limit?: number }) {
+  async execute(@Query(validationPipe) query: RequestSchema) {
     const { page, limit } = query
 
     const songs = await this.getSongUseCase.execute({
