2b05a5ba77f1262cb549fb6f32b0a5cc71c9f55a
David R Lima <david@deividy.dev>
2025-05-31 18:04:32 -0300

    publix

--- src/infra/http/controllers/youtube/youtube-oauth-callback.controller.ts
+++ src/infra/http/controllers/youtube/youtube-oauth-callback.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { GoogleAuthService } from 'src/domain/services/google-auth-service'
+import { Public } from 'src/infra/auth/public'
 
 @Controller('/youtube')
 export class YoutubeCallback {
   constructor(private readonly googleAuthService: GoogleAuthService) {}
   // In your controller
+  @Public()
   @Get('/auth/google/callback')
   async handleCallback(@Query('code') code: string) {
     const tokens = await this.googleAuthService.getTokens(code)
     return tokens
   }
 
+  @Public()
   @Get('/auth-url')
   getAuthUrl(): { url: string } {
     const url = this.googleAuthService.generateAuthUrl()
