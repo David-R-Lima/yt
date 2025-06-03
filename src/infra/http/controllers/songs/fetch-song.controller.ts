30b235afb11a9d083a3386811f8d46e5c08e690b
David R Lima <david@deividy.dev>
2025-06-02 23:18:02 -0300

    changes

--- src/infra/http/controllers/songs/fetch-song.controller.ts
+++ src/infra/http/controllers/songs/fetch-song.controller.ts
@@ -1,17 +1,29 @@
 // src/infra/http/controllers/downloaded-songs.controller.ts
 
-import { Controller, Get, Param, Res } from '@nestjs/common'
+import { Controller, Get, Param, Query, Res } from '@nestjs/common'
 import { Response } from 'express'
 import { join } from 'path'
 import { existsSync } from 'fs'
+import { Public } from 'src/infra/auth/public'
 
 const exeDir = process.cwd()
 
 @Controller('/downloaded-songs')
 export class DownloadedSongsController {
+  @Public()
   @Get(':filename')
-  getSong(@Param('filename') filename: string, @Res() res: Response) {
+  getSong(
+    @Param('filename') filename: string,
+    @Res() res: Response,
+    @Query('token') token: string
+  ) {
+    if (token !== process.env.API_KEY) {
+      return res.status(403).send('Forbidden: Invalid API key')
+    }
+
     const filePath = join(exeDir, 'downloaded-songs', filename)
+
+    console.log(`Fetching song from: ${filePath}`)
 
     if (!existsSync(filePath)) {
       return res.status(404).send('File not found')
