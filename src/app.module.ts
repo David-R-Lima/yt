d7c9ba92226c2ecf34f8a9d0100c42694a7e81ec
David R Lima <david@deividy.dev>
2025-06-02 22:04:13 -0300

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from './infra/http/http.module'
-import { ServeStaticModule } from '@nestjs/serve-static'
-import { join } from 'path'
 import { AuthModule } from './infra/auth/auth.module'
-
-const exeDir = process.cwd()
 @Module({
   imports: [AuthModule, HttpModule],
   controllers: [],
